79dd29160620128d666fc6571cd41e479993261f
Jahon A <amjahongir@gmail.com>
2018-04-13 17:01:16 -0400

    Moved the sql to the backend

--- csci320/src/main/java/Command/PrintLocation.java
+++ csci320/src/main/java/Command/PrintLocation.java
@@ -8,27 +8,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+
+/**
+ * PrintLocation Command that prints all of the location
+ */
 public class PrintLocation implements Command{
+
+    /**
+     * Execute - prints all of the available locations
+     * @param args: printlocation command
+     */
     public void execute(String [] args) {
-        try {
-
-            String query = " SELECT id, address, city, state, zipcode, country " +
-                            "FROM Store";
-
-            Scanner input = new Scanner(System.in);
-            Statement s = Sale.getConnection().createStatement();
-            ResultSet rs = s.executeQuery(query);
-            while (rs.next()) {
-                System.out.println(rs.getInt("id") + " - " +
-                        rs.getString("address") + " " + rs.getString("city") + " " +
-                        rs.getString("state") + " " + rs.getString("zipcode") + " "
-                        + rs.getString("country"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (NullPointerException ne) {
-            System.out.println("Unable to validate user. Try again.");
-        }
+        Store.printStores();
     }
 }
