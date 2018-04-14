71d841ee533cb2b7041f0f75d61e85b7d478f6c5
alexbrown <ajb2969@rit.edu>
2018-04-14 11:03:41 -0400

    added product to printCart method, starting checkout

--- csci320/src/main/java/Command/Checkout.java
+++ csci320/src/main/java/Command/Checkout.java
@@ -1,9 +1,13 @@
 package main.java.Command;
 
 
+import main.java.ShoppingCart;
+
 public class Checkout implements Command {
     public void execute(String [] args) {
         System.out.println("Checkout products.");
+        ShoppingCart sc = new Add().getCart();
+        
         // TODO Implement the backend of checkout
     }
 }
