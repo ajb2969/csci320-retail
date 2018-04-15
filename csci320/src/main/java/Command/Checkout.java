5c475723ab614de5c06aa7fe364c19a582bc2241
alexbrown <ajb2969@rit.edu>
2018-04-14 21:23:37 -0400

    fixed bug with checking out as a guest

--- csci320/src/main/java/Command/Checkout.java
+++ csci320/src/main/java/Command/Checkout.java
@@ -11,7 +11,8 @@ import java.util.HashMap;
 
 public class Checkout implements Command {
     public void execute(String [] args) {
-        args = args[0].split(" ");
+        if(args[0] != null)
+            args = args[0].split(" ");
         System.out.println("Purchasing products.");
         HashMap<Integer, Integer> sc = new Add().getCart().getCart();
 
@@ -28,77 +29,120 @@ public class Checkout implements Command {
                     String getCost = "Select price from Product where UPC = " + i;
                     ResultSet rs = s.executeQuery(getCost);
                     while(rs.next()){
-                        cost += rs.getInt("price");
+                        cost += rs.getInt("price") * sc.get(i);
                     }
                 }
 
+                if(args[0] == null){
+                    //create Sale entry for sale
+                    int storeID = 30;
+                    String getStoreID = "Select hStoreID from Customer WHERE " +
+                            "fname = \'Guest\' and lname = \'Guest\'";
+                    ResultSet rs = s.executeQuery(getStoreID);
+                    while (rs.next()) {
+                        storeID = rs.getInt("hStoreID");
+                    }
 
-                //create Sale entry for sale
 
-                int storeID = 30;
-                String getStoreID = "Select hStoreID from Customer WHERE " +
-                        "fname = \'"+ args[0] +"\' and lname = \'" + args[1] + "\'";
-                ResultSet rs = s.executeQuery(getStoreID);
-                while(rs.next()){
-                     storeID = rs.getInt("hStoreID");
+                    int getUserID = 0;
+
+                    String getSaleID = "SELECT max(saleID) FROM Sale";
+                    PreparedStatement ps = c.prepareStatement(getSaleID);
+                    rs = ps.executeQuery();
+                    int saleID = 0;
+                    while (rs.next()) {
+                        saleID = rs.getInt(1);
+                    }
+                    saleID += 1;
+                    String createSale = "Insert into Sale values (";//0 is for auto increment
+                    createSale += String.valueOf(saleID) + ",";
+                    createSale += storeID + ",";
+                    createSale += cost + ",";
+                    createSale += getUserID + ",";
+                    createSale += "?,?);";
+                    ps = c.prepareStatement(createSale);
+
+                    ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                    ps.setDate(2, new Date(System.currentTimeMillis()));
+
+                    ps.executeUpdate();
+
+                    //saleID, storeID, cost, customerID, saleTime(timestamp), date(Date)
+
+                    //add ProductSold entry connected to Sale
+                    String insertProducts = "Insert into ProductSold values(";
+
+                    for (Integer i : sc.keySet()) {
+                        insertProducts += String.valueOf(saleID) + "," +
+                                String.valueOf(i) + "," +
+                                String.valueOf(sc.get(i)) + "),(";
+                    }
+                    insertProducts = insertProducts.substring(0, insertProducts.length() - 2);
+                    ps = c.prepareStatement(insertProducts);
+                    ps.executeUpdate();
+                    NumberFormat format = NumberFormat.getCurrencyInstance();
+                    System.out.println("Your total is " + format.format(cost) + "\n");
+                    new Add().getCart().emptyCart();
+                }
+                else {
+                    //create Sale entry for sale
+                    int storeID = 30;
+                    String getStoreID = "Select hStoreID from Customer WHERE " +
+                            "fname = \'" + args[0] + "\' and lname = \'" + args[1] + "\'";
+                    ResultSet rs = s.executeQuery(getStoreID);
+                    while (rs.next()) {
+                        storeID = rs.getInt("hStoreID");
+                    }
+
+
+                    String getUserID = "Select ID from Customer where " +
+                            "fname = \'" + args[0] + "\' and lname = \'" + args[1] + "\'";
+
+                    String userID = "";
+                    rs = s.executeQuery(getUserID);
+                    while (rs.next()) {
+                        userID = rs.getString("ID");
+                    }
+                    String getSaleID = "SELECT max(saleID) FROM Sale";
+                    PreparedStatement ps = c.prepareStatement(getSaleID);
+                    rs = ps.executeQuery();
+                    int saleID = 0;
+                    while (rs.next()) {
+                        saleID = rs.getInt(1);
+                    }
+                    saleID += 1;
+                    String createSale = "Insert into Sale values (";//0 is for auto increment
+                    createSale += String.valueOf(saleID) + ",";
+                    createSale += storeID + ",";
+                    createSale += cost + ",";
+                    createSale += userID + ",";
+                    createSale += "?,?);";
+                    ps = c.prepareStatement(createSale);
+
+                    ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+                    ps.setDate(2, new Date(System.currentTimeMillis()));
+
+                    ps.executeUpdate();
+
+                    //saleID, storeID, cost, customerID, saleTime(timestamp), date(Date)
+
+                    //add ProductSold entry connected to Sale
+                    String insertProducts = "Insert into ProductSold values(";
+
+                    for (Integer i : sc.keySet()) {
+                        insertProducts += String.valueOf(saleID) + "," +
+                                String.valueOf(i) + "," +
+                                String.valueOf(sc.get(i)) + "),(";
+                    }
+                    insertProducts = insertProducts.substring(0, insertProducts.length() - 2);
+                    ps = c.prepareStatement(insertProducts);
+                    ps.executeUpdate();
+                    NumberFormat format = NumberFormat.getCurrencyInstance();
+                    System.out.println("Your total is " + format.format(cost) + "\n");
+                    new Add().getCart().emptyCart();
                 }
 
 
-
-                String getUserID = "Select ID from Customer where "+
-                        "fname = \'"+ args[0] +"\' and lname = \'" + args[1] + "\'";
-
-                String userID = "";
-                rs = s.executeQuery(getUserID);
-                while(rs.next()){
-                    userID = rs.getString("ID");
-                }
-                String getSaleID = "Select max(saleID) from Sale";
-                PreparedStatement ps = c.prepareStatement(getSaleID);
-                rs = ps.executeQuery();
-                int saleID = 0;
-                while(rs.next()){
-                    saleID = rs.getInt(1);
-                }
-                saleID +=1;
-                String createSale = "Insert into Sale values (";//0 is for auto increment
-                createSale += String.valueOf(saleID) + ",";
-                createSale += storeID +",";
-                createSale += cost + ",";
-                createSale += userID + ",";
-                createSale += "?,?);";
-                ps = c.prepareStatement(createSale);
-
-                ps.setTimestamp(1,new Timestamp(System.currentTimeMillis()));
-                ps.setDate(2,new Date(System.currentTimeMillis()));
-
-                ps.executeUpdate();
-
-                //saleID, storeID, cost, customerID, saleTime(timestamp), date(Date)
-
-/*
-                saleID = 0;
-                ps = c.prepareStatement("Select max(saleID) from Sale");
-                rs = ps.executeQuery();
-                while(rs.next()){
-                    saleID = rs.getInt(1);
-                }
-*/
-
-                //add ProductSold entry connected to Sale
-                String insertProducts = "Insert into ProductSold values(";
-
-                for (Integer i : sc.keySet()){
-                    insertProducts+= String.valueOf(saleID) + ","+
-                            String.valueOf(i) + ","+
-                            String.valueOf(sc.get(i)) + "),(";
-                }
-                insertProducts = insertProducts.substring(0,insertProducts.length()-2);
-                ps = c.prepareStatement(insertProducts);
-                ps.executeUpdate();
-                NumberFormat format = NumberFormat.getCurrencyInstance();
-                System.out.println("Your total is " + format.format(cost) +"\n");
-                new Add().getCart().emptyCart();
 
             } catch (SQLException e) { e.printStackTrace(); }
     }
